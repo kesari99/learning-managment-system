c13987cba62c53924dcad431a89be1b7c7c7a6d1
Kesari Karna <kesaridevaraya@gmail.com>
2025-04-08 19:54:19 +0530

    added backend deployed url to frontend

--- client/src/api/axiosInstance.js
+++ client/src/api/axiosInstance.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-    baseURL:'http://localhost:5000/',
+    // baseURL:'http://localhost:5000/',
+    baseURL:'https://learning-managment-system-backend.onrender.com/',
 })
 
 axiosInstance.interceptors.request.use((config) => {
