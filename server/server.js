f1d8da5356461be8b251ea4f0ef8ff5f3849043c
Kesari Karna <kesaridevaraya@gmail.com>
2025-04-08 19:43:38 +0530

    added frontend deployed link

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: ["http://localhost:5173","https://learning-managment-system-frontend.onrender.com"],
       methods: "GET,POST,PUT,DELETE",
       allowedHeaders: "Content-Type,Authorization",
     })
